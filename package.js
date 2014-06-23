1fda18bd04f10487b18c66e96698d8ac92e38e7f
Andrew Toelle <andrew.toelle@gmail.com>
2014-06-23 14:58:16 -0500

    move lens.js into lib dir

--- package.js
+++ package.js
@@ -2,21 +2,23 @@ Package.describe({
   summary:'A meteor package for displaying nlm data with the elife lens viewer.'
 });
 
-Package.on_use(function (api, where) {
-  api.export('lensDeps');
-  api.add_files('lens.js', ['client']);
+Npm.depends({
+    "substance-util": "0.2.0"
+    //"substance-surface": "0.4.0-1",
+    //"substance-operator": "0.3.0-1",
+    //"substance-document": "0.6.0-1",
+    //"substance-data": "0.9.0-1",
+    //"substance-chronicle": "0.3.0-1",
+    //"substance-application": "0.2.0-1",
+    //"lens-article": "0.1.0",
+    //"lens-reader": "0.1.0",
+    //"lens-outline": "0.2.0",
+    //"lens-converter": "0.1.0"
 });
 
-Npm.depends({
-    "substance-util": "0.2.0",
-    "substance-surface": "0.4.0-1",
-    "substance-operator": "0.3.0-1",
-    "substance-document": "0.6.0-1",
-    "substance-data": "0.9.0-1",
-    "substance-chronicle": "0.3.0-1",
-    "substance-application": "0.2.0-1",
-    "lens-article": "0.1.0",
-    "lens-reader": "0.1.0",
-    "lens-outline": "0.2.0",
-    "lens-converter": "0.1.0"
+Package.on_use(function (api, where) {
+  //api.use('npm');
+  api.add_files('lib/lens.js', ['server']);
+  api.export('lensDeps');
 });
+
